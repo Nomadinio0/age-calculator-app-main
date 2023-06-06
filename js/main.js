e57801b0d11737716d17e56550aac6bce82ed80e
Artur <a.wolniak01@gmail.com>
2023-06-07 00:07:20 +0200

    inputs red when empty

--- js/main.js
+++ js/main.js
@@ -10,16 +10,35 @@ const submitBtn = document.querySelector('.icon')
 
 const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-// naprawic przeliczanie dni (np. przy luty 28 dni)
+// const setError = (element, message) => {
+// DO ZROBIENIA
+// }
+
+function validate() {
+	// debugger
+	const dayInputError = dayInput.value.trim()
+	const monthInputError = monthInput.value.trim()
+	const yearInputError = yearInput.value.trim()
+	const inputs = document.querySelectorAll('.inputField')
+
+	inputs.forEach(input => {
+		if (input.value == '') {
+			// const error = document.querySelector('.input_day')
+			const error = input.parentElement.classList.add('invalid')
+		} else {
+			const error = input.parentElement.classList.remove('invalid')
+		}
+	})
+}
 
 function calculate() {
+	validate()
 	const date = new Date()
 	let day = date.getDate()
 	let month = 1 + date.getMonth()
 	let year = date.getFullYear()
 
 	if (dayInput.value > day) {
-		// debugger
 		dayTemp = months[monthInput.value - 1] - dayInput.value
 		day = day + dayTemp
 		month = month - 1
